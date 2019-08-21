e079bf622827d848dad23296a5dc92fef662cd21
1363343075hgy <1363343075@qq.com>
2019-08-21 12:01:10 +0800

    广告头像项目

--- src/scala/text.scala
+++
@@ -1,9 +0,0 @@
-import org.apache.spark.SparkConf
-import org.apache.spark.sql.SQLContext
-
-object text {
-  def main(args: Array[String]): Unit = {
-    val conf = new SparkConf().setAppName(this.getClass.getName).setMaster("local[*]")
-
-  }
-}
---
+++ src/scala/utils/Utils2Type.scala
@@ -0,0 +1,21 @@
+package utils
+
+object Utils2Type {
+//String转换Int
+  def toInt(str:String):Int= {
+    try {
+      str.toInt
+    } catch {
+      case _: Exception => 0
+    }
+  }
+    //String 转换Double
+    def toDouble(str:String):Double ={
+      try{
+        str.toDouble
+      }catch {
+        case _ :Exception => 0.0
+      }
+
+    }
+}
